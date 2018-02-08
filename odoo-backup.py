3112b72667599700c541551b0273e1cfcb390229
amir <amir.anwar.said@gmail.com>
2018-02-08 09:58:59 +0200

    updated the script docstring

--- odoo-backup.py
+++ odoo-backup.py
@@ -8,23 +8,7 @@ Author: Amir Anwar
 Email: amir.anwar.said@gmail.com
 Github: https://github.com/amir511
 
-
-Usage
-=====
-* You need to have an already configured git repository to put backups in it, and you should have a clone of the backup on your server.
-* You should make sure that this repo have your credentials stored so that the script will be able to push automatically without your intervention, this can be done as follow:
-    * cd to your repo directory
-    * run `git config credential.helper store`
-    * execute `git push`
-    * you will be asked for your username and password for the last time in this repository.
-* You have to simply specify the following variables in the script top:
-    * REPO_PATH
-    * BACKUP_FREQUENCY
-* Copy your configured script to your server, preferably to : 
-    
-    `/opt/odoo-autobackup/`
-    
-* You can configure this script to run as a startup service whenever your server starts, this can be done in many ways.
+Usage: Check the readme file for usage details
 """
 
 # Constants:
